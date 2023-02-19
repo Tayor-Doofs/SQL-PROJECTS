993c21cd14967d9b852be9bd23155f60469b036e
Odutayo Odufuwa <tayodufuwa@gmail.com>
2023-02-19 23:55:14 +0100

    added solutions to the Pizza Metrics questions

---
+++ 8weeksqlchallenge/Week2_Pizza_Runner/Pizza Metrics.sql
@@ -0,0 +1,36 @@
+-- SOLUTIONS TO PIZZA METRICS
+
+-- QUESTION 1
+-- How many pizzas were ordered?
+
+SELECT COUNT(pizza_id) AS pizza_ordered_count
+FROM pizza_runner.customer_orders;
+
+-- QUESTION 2
+-- How many unique customer orders were made?
+
+
+
+-- QUESTION 3
+-- How many successful orders were delivered by each runner?
+
+-- QUESTION 4
+-- How many of each type of pizza was delivered?
+
+-- QUESTION 5
+-- How many Vegetarian and Meatlovers were ordered by each customer?
+
+-- QUESTION 6
+-- What was the maximum number of pizzas delivered in a single order?
+
+-- QUESTION 7
+-- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+-- QUESTION 8
+-- How many pizzas were delivered that had both exclusions and extras?
+
+-- QUESTION 9
+-- What was the total volume of pizzas ordered for each hour of the day?
+
+-- QUESTION 10
+-- What was the volume of orders for each day of the week?
