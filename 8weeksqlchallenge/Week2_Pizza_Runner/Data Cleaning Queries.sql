b404078ef9e226dada28faa140b49670b3bf4b44
Odutayo Odufuwa <tayodufuwa@gmail.com>
2023-02-21 02:49:23 +0100

    Pizza metrics questions fully answered. The case study 2 deck has also been completed for upload

--- 8weeksqlchallenge/Week2_Pizza_Runner/Data Cleaning Queries.sql
+++ 8weeksqlchallenge/Week2_Pizza_Runner/Data Cleaning Queries.sql
@@ -1,7 +1,8 @@
 -- DATA CLEANING
 
--- The query below changes blank cells and cells equal to "null"
--- in the exclusions and extras column to NULL
+-- The query below changes blank cells and cells equal to "null" in
+-- the exclusions and extras columns found in the customer orders
+-- table to NULL
 
 UPDATE customer_orders
 SET exclusions = NULL
@@ -10,3 +11,23 @@ WHERE exclusions = "null" OR exclusions = "";
 UPDATE customer_orders
 SET extras = NULL
 WHERE extras = "null" OR extras = "";
+
+-- The query below changes blank cells and cells equal to "null" in
+-- the pickup_time, distance, duration, and cancellation columns found
+-- in the runner_orders table to NULL
+
+UPDATE runner_orders
+SET pickup_time = NULL
+WHERE pickup_time = "null" OR pickup_time = "";
+
+UPDATE runner_orders
+SET distance = NULL
+WHERE distance = "null" OR distance = "";
+
+UPDATE runner_orders
+SET duration = NULL
+WHERE duration = "null" OR duration = "";
+
+UPDATE runner_orders
+SET cancellation = NULL
+WHERE cancellation = "null" OR cancellation = "";
